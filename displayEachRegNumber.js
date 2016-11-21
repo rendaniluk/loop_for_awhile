b9f9151e2a461cd97f07213939c683e17e176c03
rendaniluk <lukhwa11582924@gmail.com>
2016-11-21 10:05:09 +0200

    Display Each registration in new line

---
+++ displayEachRegNumber.js
@@ -0,0 +1,15 @@
+//importing assert method to test function to js
+const assert = require('assert')
+//Declaring function displayEachRegNumber
+function displayEachRegNumber(RegNum){
+  //Declaring variable called Reg that split the string RegNum.
+      var Reg = RegNum.split(',');
+      //for loop to list registration numbers
+      for (var i = 0; i < Reg.length; i++){
+        //printing the list registration numbers
+        console.log(Reg[i]);
+      }
+  return Reg;
+}
+//testing code using assert
+assert.equal(displayEachRegNumber('CJ 007,CA 7846,CF 332,CY 774'),'CJ 007,CA 7846,CF 332,CY 774')
