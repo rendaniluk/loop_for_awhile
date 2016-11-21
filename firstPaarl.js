a814c41063724c6ad535d8b16646554878338988
rendaniluk <lukhwa11582924@gmail.com>
2016-11-21 14:01:41 +0200

    Registration firstPaarl

---
+++ firstPaarl.js
@@ -0,0 +1,25 @@
+//importing assert function to js
+const assert = require('assert')
+
+//Declaring function firstPaarl
+function firstPaarl(PaarlReg){
+
+//Declaring the string Paarl separated by comma
+  var Paarl = PaarlReg.split(',');
+
+  //using for loop to loop from 0 to length of string Paarl
+  for (var i = 0; i < Paarl.length; i++){
+
+    //using conditional statement to print registration from Paarl
+    if (Paarl[i].startsWith('CJ')){
+      console.log(Paarl[i]);
+    }/*else {
+      console.log('not from Paarl');
+    }*/
+  }
+  //console.log(Paarl);
+  return Paarl;
+}
+
+//using assert to test the function
+assert.deepEqual(firstPaarl('CJ 551,CY 223,CA 996,CJ 100'),[ 'CJ 551', 'CY 223', 'CA 996', 'CJ 100' ])
