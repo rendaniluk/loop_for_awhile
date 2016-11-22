917c6d9ffdc24c5d26224ab9e1d64b36cee8eb27
rendaniluk <lukhwa11582924@gmail.com>
2016-11-22 11:33:26 +0200

    Modified allFromTown function

--- allFromTown.js
+++ allFromTown.js
@@ -1,17 +1,17 @@
 //importing assert to js
 const assert = require('assert')
-//Declaring an empty array
-var whereFrom = [];
 //Declaring function allFromTown
-function allFromTown(Regnum,town){
+function allFromTown(regNum,town){
+  //Declaring an empty array
+  var whereFrom = [];
   //split string using .split
-   var WhichTown = Regnum.split(',');
-  //using for loop to loop from 0 to WhichTown length
-  for (var i = 0;i < WhichTown.length;i++){
-    //using conditional statement to print WhichTown registration
-    if(WhichTown[i].startsWith(town)){
-      whereFrom.push(WhichTown[i]);
-      console.log(WhichTown[i]);
+   var whichTown = regNum.split(',');
+  //using for loop to loop from 0 to whichTown length
+  for (var i = 0;i < whichTown.length;i++){
+    //using conditional statement to print whichTown registration
+    if(whichTown[i].startsWith(town)){
+      whereFrom.push(whichTown[i]);
+      // console.log(whichTown[i]);
     }
   }
   console.log(whereFrom);
@@ -19,7 +19,7 @@ function allFromTown(Regnum,town){
 }
 //Calling function allFromTown store value store its value to fromStellies
 var fromStellies = allFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL');
-//var fromKuilsriver = allFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CF');
-//testing using assert function
 assert.deepEqual(fromStellies, ['CL 124', 'CL 345', 'CL 341'])
-//assert.deepEqual(fromKuilsriver, []);
+//testing using assert function
+var fromKuilsriver = allFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CF');
+assert.deepEqual(fromKuilsriver, []);
