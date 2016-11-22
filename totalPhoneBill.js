11f574b6e2a08803e7810f7c43ef1675a18024b1
rendaniluk <lukhwa11582924@gmail.com>
2016-11-22 14:18:54 +0200

    total phone bill

---
+++ totalPhoneBill.js
@@ -0,0 +1,31 @@
+//importing assert to js
+const assert = require('assert')
+//Declaring function totalPhoneBill
+function totalPhoneBill(airtime,sT){
+  //Declaring an empty array
+  var howMuch = [];
+  //split string using .split
+   var bilL = airtime.split(',');
+  //using for loop to loop from 0 to bilL length
+  for (var i = 0;i < bilL.length;i++){
+    //using conditional statement to print bilL
+        if(bilL[i].startsWith(sT)){
+      howMuch.push(bilL[i]);
+      //console.log(bilL[i]);
+    }
+  }
+  //console.log(howMuch.length);
+  return howMuch.length;
+}
+//Calling function totalPhoneBill store its value to checkCall and checkSms
+var checkCall = totalPhoneBill('call,sms,call,sms,sms','ca');
+var checkSms = totalPhoneBill('call,sms,call,sms,sms','sm');
+//calculating total cost for call and sms
+var totcal = checkCall*2.75;
+var totsms = checkSms*0.65;
+//summing totcal and totsms
+var alltogether = totsms + totcal;
+//printing variable alltogether
+console.log(alltogether);
+//testing using assert function
+assert.deepEqual(alltogether,7.45)
