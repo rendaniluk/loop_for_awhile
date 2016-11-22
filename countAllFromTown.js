9707b42ef939875857d1989a7972381bbd45aa56
rendaniluk <lukhwa11582924@gmail.com>
2016-11-22 11:37:58 +0200

    Modified countAllFromTown function

--- countAllFromTown.js
+++ countAllFromTown.js
@@ -1,9 +1,9 @@
 //importing assert to js
 const assert = require('assert')
-//Declaring an empty array
-var HowMany = [];
 //Declaring function allFromTown
 function allFromTown(Regnum,town){
+  //Declaring an empty array
+  var HowMany = [];
   //split string using .split
    var WhichTown = Regnum.split(',');
   //using for loop to loop from 0 to WhichTown length
@@ -19,7 +19,7 @@ function allFromTown(Regnum,town){
 }
 //Calling function allFromTown store value store its value to fromStellies
 var fromStellies = allFromTown('CL 124,CY 567,CL 441,CL 345, CJ 456,CL 341','CL');
-//var fromKuilsriver = allFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CF');
+var fromKuilsriver = allFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CF');
 //testing using assert function
 assert.deepEqual(fromStellies, 4)
-//assert.deepEqual(fromKuilsriver, []);
+assert.deepEqual(fromKuilsriver, 0);
