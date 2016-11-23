b0908ab21c8419bbe8b5c0b8567b3f1430fa12e8
rendaniluk <lukhwa11582924@gmail.com>
2016-11-23 13:06:43 +0200

    Function that return shortest word

---
+++ shortestWord.js
@@ -0,0 +1,21 @@
+//importing assert function to JS
+const assert = require('assert')
+//Declaring the shortestWord function
+function shortestWord(sentence) {
+//Declaring variables used in the function shortestWord
+  var wordSeparator = sentence.split(' ');
+  var shortestLenth = wordSeparator.length;
+  var shoterWord = ' ';
+  //for loop to run from 0 to length of string sentence
+  for (var i=0;i<wordSeparator.length;i++) {
+    if (wordSeparator[i].length < shortestLenth) {
+        shoterWord = wordSeparator[i];
+    }
+  }
+  return shoterWord;
+}
+//Calling and storing function call in variable whats_short
+var whats_short = shortestWord('Love writing JavaScript I')
+console.log(whats_short);
+//using assert methods to test function shortestWord
+assert.deepEqual(whats_short,'I')
